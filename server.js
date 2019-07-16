bd11edecd2944fe09518d2ef1b0c22fe78c3c0bc
Marion <marion_ott@me.com>
2019-07-16 19:13:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const socketIo = require('socket.io')
+const http = require('http')
+
+const port = 9000
+const app = express()
+
+const server = http.createServer(app)
+
+const io = socketIo(server)
+
+
+io.on('connection', socket => {
+    console.log('somebody is connected');
+    socket.on('event', (data) => {
+        socket.emit('event', 'plouf')
+    })
+})
+
+
+server.listen(9000, function(){
+    console.log('listening on *:3000');
+});
+
+
+// app.listen(port, () => console.log('listening'))
