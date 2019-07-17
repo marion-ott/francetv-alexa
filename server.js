f53d73f2f5d532fce8f91fbeee0b46f4ff8dd989
Marion <marion_ott@me.com>
2019-07-17 18:51:40 +0200

    add new request

--- server.js
+++ server.js
@@ -16,7 +16,63 @@ const io = socketIo(server)
 app.get('/', (req, res) => {
     const data = {
         state: 'home',
-        speech: 'Cette application vous permet de rejoindre un live sportif commenté par un tiers ou de vous glisser vous-même dans la peau d\'un commentateur.'
+        speech: 'Cette application vous permet de rejoindre un live sportif commenté par un tiers, ou de vous glisser vous-même dans la peau d\'un commentateur.'
+    }
+    io.emit('event', data)
+    res.json(data)
+})
+
+
+const streamList = [
+    [
+        {
+            name: 'Bill',
+            image: 'image.jpg'
+        },
+        {
+            name: 'Bob',
+            image: 'image.jpg'
+        },
+        {
+            name: 'Gérard',
+            image: 'image.jpg'
+        }
+    ],
+    [
+        {
+            name: 'Paule',
+            image: 'image.jpg'
+        },
+        {
+            name: 'Hélène',
+            image: 'image.jpg'
+        },
+        {
+            name: 'Marion',
+            image: 'image.jpg'
+        }
+    ]
+]
+
+
+app.get('/stream/:id', (req, res) => {
+    const id = req.params.id
+    const data = {
+        state: 'streamList',
+        data: streamList[id],
+        speech: `Choisissez un streamer parmi : ${streamList[id][0].name}, ${streamList[id][1].name}, ou ${streamList[id][2].name}`
+    }
+    io.emit('event', data)
+    res.json(data)
+})
+
+
+app.get('/media/:streamer', (req, res) => {
+    const id = req.params.id
+    const data = {
+        state: 'streamList',
+        data: 'video.mp4',
+        speech: `Voici le stream de ${req.params.id}`
     }
     io.emit('event', data)
     res.json(data)
